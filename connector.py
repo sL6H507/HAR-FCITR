d2c739a81d02f89131262f0087a489651bf68006
sL6H507 <62222489+sL6H507@users.noreply.github.com>
2024-07-09 04:09:00 +0300

    Update connector.py

--- connector.py
+++ connector.py
@@ -2,9 +2,9 @@ import mysql.connector
 import os
 
 mydb = mysql.connector.connect(
-  host="",
-  user="",
-  password='bKKWss1YA2xaEsEpj73l',
+  host="database-1.cz48cuo4sfq2.us-east-1.rds.amazonaws.com",
+  user="fcitrproject3",
+  password='dEqAze6u',
   database="fcitrprojectkh"
 )
 cursor = mydb.cursor()
