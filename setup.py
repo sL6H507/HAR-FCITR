c9357651ed17ab25d579801a05b68143bb90fed2
sL6H507 <62222489+sL6H507@users.noreply.github.com>
2024-07-10 12:26:14 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,53 +1,43 @@
 import subprocess
 import sys
 import tkinter as tk
-from tkinter import Canvas, messagebox
+from tkinter import Canvas, Label, messagebox
 
 class Setup(tk.Frame):
     def __init__(self, parent):
-        tk.Frame.__init__(self, parent)
+        tk.Frame.__init__(self, parent, bg="#FFFFFF", width=300, height=200)
+        
         self.canvas = Canvas(
             self,
             bg="#FFFFFF",
-            height=1024,
-            width=1440,
+            height=300,
+            width=200,
             bd=0,
             highlightthickness=0,
             relief="ridge"
         )
-        check_button = tk.Button(self, text="Check Required Packages", command=self.check_packages)
-        check_button.pack(pady=20)
-        self.canvas.place(x=0, y=0)
+        self.canvas.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+
+        button_1 = tk.Button(self, text="Check Required Packages", command=self.check_packages, font=("Inter", 10), relief="flat")
+        button_1.place(relx=0.5, rely=0.6, anchor=tk.CENTER, width=200, height=50)
+
+        welcome_label = Label(self, text="Welcome To Our Project\n", font=("Inter", 12),bg="#ffffff")
+        welcome_label.place(relx=0.5, rely=0.3, anchor=tk.CENTER)
 
     def get_requirements(self):
         return [
-            'absl-py==2.1.0', 'aiofiles==23.2.1', 'altgraph==0.17.4', 'anyio==4.3.0', 
-            'argcomplete==3.3.0', 'attrs==23.2.0', 'beautifulsoup4==4.12.3', 'bleach==6.1.0', 
-            'boto3==1.34.101', 'botocore==1.34.101', 'brotli==1.1.0', 'cachetools==5.3.3', 
-            'cython==3.0.10', 'dacite==1.7.0', 'defusedxml==0.7.1', 'deprecated==1.2.14', 
-            'dill==0.3.8', 'dnspython==2.6.1', 'fastjsonschema==2.19.1', 'fiftyone==0.23.8', 
-            'fiftyone-brain==0.16.1', 'fiftyone-db==1.1.2', 'fiftyone-desktop==0.33.7', 
-            'ftfy==6.2.0', 'future==1.0.0', 'glob2==0.7', 'graphql-core==3.2.3', 'grpcio==1.63.0', 
-            'h11==0.14.0', 'h2==4.1.0', 'hpack==4.0.0', 'httpcore==1.0.5', 'httpx==0.27.0', 
-            'humanize==4.9.0', 'hypercorn==0.16.0', 'hyperframe==6.0.1', 'imageio==2.34.1', 
-            'inflate64==1.0.0', 'jmespath==1.0.1', 'joblib==1.4.2', 'jsonlines==4.0.0', 
-            'jupyterlab-pygments==0.3.0', 'kaleido==0.2.1', 'lapx==0.5.9', 'lazy-loader==0.4', 
-            'markdown==3.6', 'mistune==3.0.2', 'mongoengine==0.24.2', 'motor==3.4.0', 
-            'multivolumefile==0.2.3', 'nbclient==0.10.0', 'nbconvert==7.16.4', 'nbformat==5.10.4', 
-            'opencv-python==4.9.0.80', 'pafy==0.5.5', 'pandas==2.2.2', 'pandocfilters==1.5.1', 
-            'pefile==2023.2.7', 'pickleshare==0.7.5', 'plotly==5.22.0', 'pprintpp==0.4.0', 
-            'priority==2.0.0', 'protobuf==5.26.1', 'py7zr==0.21.0', 'pybcj==1.0.2', 'pycocotools==2.0', 
-            'pycryptodomex==3.20.0', 'pyinstaller==6.6.0', 'pyinstaller-hooks-contrib==2024.6', 
-            'pymongo==4.7.2', 'pyppmd==1.1.0', 'pywin32-ctypes==0.2.2', 'pyzstd==0.15.10', 
-            'rarfile==4.2', 'regex==2024.4.28', 'retrying==1.3.4', 's3transfer==0.10.1', 
-            'scikit-image==0.23.2', 'scikit-learn==1.4.2', 'sniffio==1.3.1', 'sortedcontainers==2.4.0', 
-            'soupsieve==2.5', 'sse-starlette==0.10.3', 'sseclient-py==1.8.0', 'starlette==0.37.2', 
-            'strawberry-graphql==0.138.1', 'tabulate==0.9.0', 'tenacity==8.3.0', 'tensorboard==2.16.2', 
-            'tensorboard-data-server==0.7.2', 'texttable==1.7.0', 'threadpoolctl==3.5.0', 'tifffile==2024.5.3', 
-            'tinycss2==1.3.0', 'torchaudio==2.3.0', 'torchvision==0.18.0', 'tzlocal==5.2', 
-            'universal-analytics-python3==1.1.1', 'voxel51-eta==0.12.6', 'wcwidth==0.2.13', 
-            'webencodings==0.5.1', 'werkzeug==3.0.3', 'wrapt==1.16.0', 'wsproto==1.2.0', 'xmltodict==0.13.0', 
-            'youtube-dl==2020.12.2'
+            "mysql-connector-python",
+            "Pillow",
+            "requests",
+            "opencv-python",
+            "ultralytics",
+            "roboflow",
+            "nvsmi",
+            "tensorflow",
+            "tensorboard",
+            "torch",
+            "torchvision",
+            "torchaudio"
         ]
 
     def check_installed_packages(self, requirements):
@@ -80,17 +70,29 @@ class Setup(tk.Frame):
         missing_packages = self.check_installed_packages(requirements)
         
         if not missing_packages:
-            messagebox.showinfo("Result", "All required packages are installed.")
+            messagebox.showinfo("Result", "All required packages are installed.\nFor GPU install Cuda toolkit first")
+            self.quit()
         else:
             result = "The following packages are missing:\n" + "\n".join(missing_packages)
             messagebox.showwarning("Result", result)
             self.install_packages(missing_packages)
-            messagebox.showinfo("Result", "Missing packages have been installed.")
-            self.quit()  # Close the setup window after checking/installing packages
+            messagebox.showinfo("Result", "Missing packages have been installed.\nFor GPU install Cuda toolkit first")
+            self.quit()
+
+def center_window(root, width, height):
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    x = (screen_width // 2) - (width // 2)
+    y = (screen_height // 2) - (height // 2)
+    root.geometry(f'{width}x{height}+{x}+{y}')
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.geometry("480x480")
+    root.geometry("300x200")
+    root.resizable(False, False)    
     app = Setup(root)
-    app.pack(fill="both", expand=True)
+    app.pack(fill="both", expand=False)
+    
+    center_window(root, 300, 200)
+    
     root.mainloop()
