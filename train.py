fbf4fc8eb21f0eb7bfbcefbb0160f0bb4f9b8478
sL6H507 <62222489+sL6H507@users.noreply.github.com>
2024-07-11 03:21:58 +0300

    default dataset

--- train.py
+++ train.py
@@ -1,9 +1,9 @@
 import tkinter as tk
 from tkinter import Canvas, Button, filedialog, OptionMenu, Text, Radiobutton, messagebox
 from utils import fetch_image,fetch_and_display_image
-import threading,subprocess,sys,home,predict,torch
-from ultralytics import YOLO  # Assuming YOLO class is imported from ultralytics
-import nvsmi  # Assuming nvsmi module is imported for GPU info
+import threading,subprocess,sys,home,predict,torch,nvsmi,os,yaml
+from ultralytics import YOLO
+from roboflow import Roboflow
 
 class Train(tk.Frame):
     def __init__(self, parent, controller):
@@ -11,7 +11,9 @@ class Train(tk.Frame):
         self.controller = controller
         self.selected_device = tk.StringVar()
         self.selected_device.set(None)
-        self.value_inside = tk.StringVar(self) 
+        self.value_model_inside = tk.StringVar(self) 
+        self.value_dataset_inside = tk.StringVar(self)
+        self.rf = Roboflow(api_key="HFgIJHLGYqUPmOtgYZ1m")
         self.configure(bg="#FFFFFF")
         self.canvas = Canvas(
             self,
@@ -29,23 +31,26 @@ class Train(tk.Frame):
         
         self.image_image_1 = fetch_and_display_image(self,"https://media.tenor.com/Q8nIjfXA_awAAAAi/fcitr.gif", 115.0, 107.0,214,209)
 
-        predict_page_btn = tk.Button(self, text="For Predicting Click Here", font=("Inter", 15),justify="left", borderwidth=0, highlightthickness=0,
+        predict_page_btn = tk.Button(self, text="Predicting", font=("Inter", 15),justify="left", borderwidth=0, highlightthickness=0,bg="#FFFFFF",
                                      command=lambda: controller.show_frame_with_auth(predict.Predict), relief="flat")
-        predict_page_btn.place(x=594.0, y=905.0, width=259.0, height=82.0)
+        predict_page_btn.place(x=25.0, y=911.0, width=259.0, height=82.0)
 
         self.Training_btn = tk.Button(self, text="Train", font=("Inter", 20), borderwidth=0, highlightthickness=0,
                                       command=self.start_training, relief="flat")
         self.Training_btn.place(x=659.0, y=448.0, width=148.0, height=39.0)
 
-        self.entry_1 = tk.Entry(self, bd=0, bg="#F8F8F8", fg="#000716", highlightthickness=0)
-        self.entry_1.place(x=316.0, y=384.0, width=502.0, height=43.0)
-        self.canvas.create_text(316.0, 341.0, anchor="nw", text="Dataset (.yaml) location :", fill="#000000",
-                                font=("Inter", 36 * -1))
+        #self.entry_1 = tk.Entry(self, bd=0, bg="#F8F8F8", fg="#000716", highlightthickness=0)
+        #self.entry_1.place(x=316.0, y=384.0, width=502.0, height=43.0)
+        self.canvas.create_text(316.0, 341.0, anchor="nw", text="Select The Dataset (.yaml):", fill="#000000",
+                                font=("Inter", 32 * -1))
+        self.value_dataset_inside.set("Select Dataset") 
+        options_dataset_list = ["Human Action Recognition", "Other"] 
 
-        self.button_image_3 = fetch_image(self,"https://media.tenor.com/kEvgsQa811YAAAAM/dir.gif", 70, 36)
-        datafile_btn = tk.Button(self, image=self.button_image_3, borderwidth=0, highlightthickness=0,
-                                 command=self.select_data_file, relief="flat")
-        datafile_btn.place(x=818.0, y=384.0, width=70.0, height=45.0)
+        dataset_menu = tk.OptionMenu(self, self.value_dataset_inside, *options_dataset_list, command=self.change_dataset) 
+        dataset_menu.place(x=405.0, y=380.0, width=189.0, height=49.0)
+        #self.button_image_3 = fetch_image(self,"https://media.tenor.com/kEvgsQa811YAAAAM/dir.gif", 70, 36)
+        #datafile_btn = tk.Button(self, image=self.button_image_3, borderwidth=0, highlightthickness=0,command=self.select_data_file, relief="flat")
+        #datafile_btn.place(x=818.0, y=384.0, width=70.0, height=45.0)
 
         self.canvas.create_text(677.0, 47.0, anchor="nw", text="Train", fill="#000000", font=("Inter", 36 * -1))
 
@@ -72,15 +77,15 @@ class Train(tk.Frame):
         self.canvas.create_text(942.0, 147.0, anchor="nw", text="Image Size :", fill="#000000", font=("Inter", 32 * -1))
         self.canvas.create_text(738.0, 237.0, anchor="nw", text="Batch Size :", fill="#000000", font=("Inter", 32 * -1))
 
-        self.value_inside.set("Select a Pre-Trained Model") 
-        options_list = ["YoloV9 Compact", "YoloV9 Extended", "Gelan Compact", "Gelan Extended","Other"] 
+        self.value_model_inside.set("Select a Pre-Trained Model") 
+        options_model_list = ["YoloV9 Compact", "YoloV9 Extended", "Gelan Compact", "Gelan Extended","Other"] 
 
-        model_menu = tk.OptionMenu(self, self.value_inside, *options_list, command=self.change_model) 
-        model_menu.place(x=413.0, y=188.0, width=189.0, height=49.0)
+        model_menu = tk.OptionMenu(self, self.value_model_inside, *options_model_list, command=self.change_model) 
+        model_menu.place(x=405.0, y=227.0, width=189.0, height=49.0)
 
         TensorBoard_btn = tk.Button(self, text="TensorBoard", font=("Inter", 20), borderwidth=0, highlightthickness=0,
                                     command=self.start_tensorboard, relief="flat")
-        TensorBoard_btn.place(x=413.0, y=275.0, width=189.0, height=49.0)
+        TensorBoard_btn.place(x=578.0, y=911.0, width=259.0, height=82.0)
 
         self.GPU = tk.Radiobutton(
             self,
@@ -126,10 +131,7 @@ class Train(tk.Frame):
             filetypes=(("Dataset information files", "*.yaml"),)
         )
         if self.data_file_path:
-            self.entry_1.delete(0, tk.END)
-            self.entry_1.insert(0, self.data_file_path)
-        print(self.data_file_path)
-        return self.data_file_path
+            return self.data_file_path
     
     def select_logdir_file(self):
         self.logdir_file = filedialog.askdirectory(
@@ -141,7 +143,6 @@ class Train(tk.Frame):
         if selection == "Other":
             self.select_model_file()
         else:
-            # Assign the appropriate pre-trained model path based on selection
             if selection == "YoloV9 Compact":
                 self.trainmodel_file_path = "yolov9c.pt"
             elif selection == "YoloV9 Extended":
@@ -154,6 +155,41 @@ class Train(tk.Frame):
                 self.trainmodel_file_path = None
                 tk.messagebox.showwarning(title="Training", message="Wrong Selection")
 
+    def change_dataset(self, selection):
+        if selection == "Other":
+            self.select_data_file()
+        else:
+            if selection == "Human Action Recognition":
+                try:
+                    project = self.rf.workspace("realtime-human-action-recognition-in-still-images-c4ciq").project("real-time-human-action-recognition")
+                    version = project.version(7)
+                    dataset = version.download("yolov9")
+                    print(dataset.model_format)
+                    self.data_file_path = os.path.join(dataset.location, "data.yaml")                     
+                    self.reorganize_data_yaml(self.data_file_path)
+                    self.data_file_path = self.data_file_path.replace("\\", "/")
+                    print(self.data_file_path)
+                except Exception as e:
+                    tk.messagebox.showerror(title="Dataset Error", message=f"An error occurred: {e}")
+            else:
+                self.data_file_path = None
+                tk.messagebox.showwarning(title="Training", message="Wrong Selection")
+
+    def reorganize_data_yaml(self,file_path):
+        with open(file_path, 'r') as file:
+            existing_data = yaml.safe_load(file)
+        updated_data = {
+            'train': '../train/images',
+            'val': '../valid/images',
+            'test': '../test/images',
+            'nc': existing_data['nc'],
+            'names': existing_data['names']
+        }
+        with open(file_path, 'w') as file:
+            yaml.safe_dump(updated_data, file, default_flow_style=False)
+
+        print(f"data.yaml updated with reorganized structure at: {file_path}")
+
     def start_training(self):
         try:
             epochs = int(self.epochs_field.get())
@@ -161,7 +197,8 @@ class Train(tk.Frame):
             img_size = int(self.imgsz_field.get())
             device = "cpu" if self.selected_device.get() == "CPU" else "0"
             if self.trainmodel_file_path and self.data_file_path and epochs and img_size and batch_size:
-                self.Training_btn.configure(state="disabled")
+                self.Training_btn.configure(state="disabled")                
+                self.entry_2.insert(tk.END, "Training will Start Now...\n")
                 command = [sys.executable, "-c", f"""
 import subprocess
 from ultralytics import YOLO
@@ -201,7 +238,7 @@ if __name__ == '__main__':
         process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, encoding='utf-8')
         
         # Read output line by line and update the GUI
-        for line in iter(process.stdout.readline, ""):
+        for line in iter(process.stdout.readline,""):
             self.entry_2.insert(tk.END, line)
             self.entry_2.see(tk.END)
             self.update_idletasks()  # Update the GUI to show new content
@@ -215,15 +252,19 @@ if __name__ == '__main__':
 
     def update_gpu_info(self):
         try:
+            # Clear previous GPU information
             self.gpu_info.delete("1.0", tk.END)
+
+            # Get GPU information
             gpu_info = self.get_gpu_info()
-            self.gpu_info.insert(tk.END, gpu_info)            
-        # Schedule the next update after 5 seconds (5000 milliseconds)
-            self.after(5000, self.update_gpu_info)
+
+            # Update text widget with new GPU information
+            self.gpu_info.insert(tk.END, gpu_info)
         except Exception as e:
             print(f"Error updating GPU info: {e}")
 
-        
+        # Schedule the next update after 5 seconds (5000 milliseconds)
+        self.after(5000, self.update_gpu_info)
 
     def get_gpu_info(self):
         try:
