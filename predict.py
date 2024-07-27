bb04949cc8ef4648a1ccf3cf0c7ff13ccadfaf36
sL6H507 <62222489+sL6H507@users.noreply.github.com>
2024-07-27 06:29:16 +0300

    Add files via upload

--- predict.py
+++ predict.py
@@ -3,20 +3,22 @@ from tkinter import Canvas, Button, filedialog, Scale, Text, messagebox
 from tkinter import simpledialog
 from PIL import Image, ImageTk
 from io import BytesIO
-from utils import fetch_and_display_image,fetch_image
+from utils import fetch_and_display_image, fetch_image
 import requests
 import cv2
-import home,train
+import home
+import train
 import wget
 import os
-from ultralytics import YOLO 
+from ultralytics import YOLO
+
 
 class Predict(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
         self.model_file_path = None
-        self.value_model_inside = tk.StringVar(self) 
+        self.value_model_inside = tk.StringVar(self)
         self.file_path = None
 
         self.configure(bg="#FFFFFF")
@@ -48,13 +50,17 @@ class Predict(tk.Frame):
             fill="#000000",
             font=("Inter", 36 * -1)
         )
-        self.canvas.create_text(304.0, 641.0, anchor="nw", text="Live Footage", fill="#000000", font=("Inter", 36 * -1))
-        self.canvas.create_text(659.0, 641.0, anchor="nw", text="Video", fill="#000000", font=("Inter", 36 * -1))
-        self.canvas.create_text(973.0, 641.0, anchor="nw", text="Image", fill="#000000", font=("Inter", 36 * -1))
+        self.canvas.create_text(
+            304.0, 641.0, anchor="nw", text="Live Footage", fill="#000000", font=("Inter", 36 * -1))
+        self.canvas.create_text(
+            659.0, 641.0, anchor="nw", text="Video", fill="#000000", font=("Inter", 36 * -1))
+        self.canvas.create_text(
+            973.0, 641.0, anchor="nw", text="Image", fill="#000000", font=("Inter", 36 * -1))
 
-        self.value_model_inside.set("Select a Model") 
-        options_model_list = ["Human Action Recogition","Other"] 
-        model_menu = tk.OptionMenu(self, self.value_model_inside, *options_model_list, command=self.change_model) 
+        self.value_model_inside.set("Select a Model")
+        options_model_list = ["Human Action Recogition", "Other"]
+        model_menu = tk.OptionMenu(
+            self, self.value_model_inside, *options_model_list, command=self.change_model)
         model_menu.place(x=385.0, y=242.0, width=189.0, height=49.0)
 
         self.canvas.create_text(
@@ -127,7 +133,8 @@ class Predict(tk.Frame):
             font=("Inter", 24 * -1)
         )
 
-        self.result_entry = Text(self, bd=0, bg="#F8F8F8", fg="#000716", highlightthickness=0)
+        self.result_entry = Text(
+            self, bd=0, bg="#F8F8F8", fg="#000716", highlightthickness=0)
         self.result_entry.place(
             x=303.0,
             y=407.0,
@@ -136,7 +143,7 @@ class Predict(tk.Frame):
         )
 
         predict_btn = tk.Button(self, text="Predict", font=("Inter", 20), borderwidth=0, highlightthickness=0,
-                                command=self.p_predict, relief="flat")
+                                command=self.imgvid_predict, relief="flat")
         predict_btn.place(
             x=956.0,
             y=242.0,
@@ -154,8 +161,8 @@ class Predict(tk.Frame):
         )
 
         linkprediction = tk.Button(self, text="Prediction using Link", font=("Inter", 15), borderwidth=0,
-                                 highlightthickness=0, command=self.show_link_prediction_dialog,
-                                 relief="flat")
+                                   highlightthickness=0, command=self.show_link_prediction_dialog,
+                                   relief="flat")
         linkprediction.place(
             x=729.0,
             y=902.0,
@@ -164,8 +171,8 @@ class Predict(tk.Frame):
         )
 
         directoryprediction = tk.Button(self, text="Prediction Directory", font=("Inter", 15), borderwidth=0,
-                                 highlightthickness=0, command=self.directoryprediction,
-                                 relief="flat")
+                                        highlightthickness=0, command=self.directoryprediction,
+                                        relief="flat")
         directoryprediction.place(
             x=436.0,
             y=902.0,
@@ -178,20 +185,26 @@ class Predict(tk.Frame):
                                       highlightthickness=0, relief="flat")
         self.homepage_btn.place(x=1289, y=931, width=131, height=50)
 
+        self.image_image_1 = fetch_and_display_image(
+            self, "https://media.tenor.com/Q8nIjfXA_awAAAAi/fcitr.gif", 115.0, 107.0, 214, 209)
 
-        self.image_image_1 = fetch_and_display_image(self,"https://media.tenor.com/Q8nIjfXA_awAAAAi/fcitr.gif", 115.0, 107.0,214,209)
+        self.photoimage = fetch_image(
+            self, "https://media.tenor.com/Zv5dIqsnvtcAAAAM/photo.gif", 221, 209),
+        predictimage = tk.Button(self, image=self.photoimage, borderwidth=0,
+                                 highlightthickness=0, command=self.select_image_file, relief="flat")
+        predictimage.place(x=914.0, y=685.0, width=221.0, height=209.0)
 
-        self.photoimage=fetch_image(self,"https://media.tenor.com/Zv5dIqsnvtcAAAAM/photo.gif",221,209),
-        predictimage = tk.Button(self, image=self.photoimage, borderwidth=0, highlightthickness=0,command=self.select_image_file, relief="flat")
-        predictimage.place(x=914.0,y=685.0,width=221.0,height=209.0)
-
-        self.liveimage = fetch_image(self,"https://media.tenor.com/LfYVxf_SjysAAAAM/cam.gif", 221, 209)
-        self.predictlive = tk.Button(self, borderwidth=0, highlightthickness=0, image=self.liveimage, command=self.cam_predict, relief="flat")
+        self.liveimage = fetch_image(
+            self, "https://media.tenor.com/LfYVxf_SjysAAAAM/cam.gif", 221, 209)
+        self.predictlive = tk.Button(self, borderwidth=0, highlightthickness=0,
+                                     image=self.liveimage, command=self.cam_predict, relief="flat")
         self.predictlive.place(x=303.0, y=685.0, width=221.0, height=209.0)
 
-        self.predictvid_img = fetch_image(self,"https://media.tenor.com/Co-LVRF8ZwEAAAAM/cam.gif", 221, 209)
-        predictvid = tk.Button(self, image=self.predictvid_img, borderwidth=0, highlightthickness=0,command=self.select_video_file, relief="flat")
-        predictvid.place(x=597.0,y=685.0,width=221.0,height=209.0)
+        self.predictvid_img = fetch_image(
+            self, "https://media.tenor.com/Co-LVRF8ZwEAAAAM/cam.gif", 221, 209)
+        predictvid = tk.Button(self, image=self.predictvid_img, borderwidth=0,
+                               highlightthickness=0, command=self.select_video_file, relief="flat")
+        predictvid.place(x=597.0, y=685.0, width=221.0, height=209.0)
 
     def select_model_file(self):
         self.model_file_path = filedialog.askopenfilename(
@@ -207,14 +220,15 @@ class Predict(tk.Frame):
         else:
             if selection == "Human Action Recogition":
                 self.result_entry.delete(1.0, tk.END)
-                
+
                 # Filename and URL
                 filename = 'HAR.pt'
                 url = 'https://download1640.mediafire.com/8hecjri417pgMbo9Xhmmb_CSa_hwrkb_LLlahvFXZAXQL3UM6yIQ24ESKqlyjLeLFgl8eeZqqECCSH2RmBb_g5uzrRZYqWATsdpkOX1faYCofk23R1oUwahfr-EFzUPmBkQfMQrNyYnk51H-bXjD4PCdLB1MjTf_5HguUkolrUc/yxuozlnqx3deya0/HAR.pt'
-                
+
                 # Check if file exists
                 if os.path.exists(filename):
-                    self.result_entry.insert(tk.END, f"File already exists: {filename}")
+                    self.result_entry.insert(
+                        tk.END, f"File already exists: {filename}")
                     self.model_file_path = filename
                 else:
                     # Download the file if it does not exist
@@ -223,22 +237,24 @@ class Predict(tk.Frame):
                     self.model_file_path = downloaded_file
             else:
                 self.model_file_path = None
-                tk.messagebox.showwarning(title="Training", message="Wrong Selection")
-
+                tk.messagebox.showwarning(
+                    title="Training", message="Wrong Selection")
 
     def select_video_file(self):
         self.file_path = filedialog.askopenfilename(
             title="Select a Video File",
-            filetypes=(("Video Files (.mp4;.avi)", "*.mp4;*.avi;*.mpeg;*.webm;*.mpg;*.mov;*.gif;*.asf;*.mkv;*.m4v"),)
+            filetypes=(("Video Files (.mp4;.avi)",
+                       "*.mp4;*.avi;*.mpeg;*.webm;*.mpg;*.mov;*.gif;*.asf;*.mkv;*.m4v"),)
         )
 
     def select_image_file(self):
         self.file_path = filedialog.askopenfilename(
             title="Select an Image File",
-            filetypes=(("Image Files (.png;.jpg;.jpeg)", "*.png;*.jpg;*.jpeg;*.dng;*.tiff;*.tif;*.webp;*.bmp;*.mpo"),)
+            filetypes=(("Image Files (.png;.jpg;.jpeg)",
+                       "*.png;*.jpg;*.jpeg;*.dng;*.tiff;*.tif;*.webp;*.bmp;*.mpo"),)
         )
 
-    def p_predict(self):
+    def imgvid_predict(self):
         confidence = float(self.confscale.get()) / 100
         iou = float(self.iouscale.get()) / 100
 
@@ -246,23 +262,25 @@ class Predict(tk.Frame):
             if self.model_file_path and self.file_path:
                 self.result_entry.delete(1.0, tk.END)  # Clear previous results
                 results = YOLO(self.model_file_path).predict(self.file_path, show=True, save=True, conf=confidence,
-                                                             iou=iou,imgsz=640)
-                results_text = f"File name {self.file_path}:\n\nSpeed {results[0].speed}:\n\nSaved Path {results[0].path}:\n"
+                                                             iou=iou, imgsz=640)
+                results_text = f"File name : {self.file_path}\n\nSpeed : {
+                    results[0].speed}\n\nSaved Path : {results[0].save_dir}\n"
                 self.result_entry.insert(tk.END, results_text)
             else:
-                messagebox.showwarning(title="Predict", message="Model file or input file not selected.")
+                messagebox.showwarning(
+                    title="Predict", message="Model file or input file not selected.")
         except Exception as er:
             messagebox.showerror(title="Predict", message=str(er))
 
-
     def cam_predict(self):
         if not self.model_file_path:
-            messagebox.showwarning(title="Predict", message="Model file not selected.")
+            messagebox.showwarning(
+                title="Predict", message="Model file not selected.")
             return
         model = YOLO(model=self.model_file_path)
         confidence = float(self.confscale.get()) / 100
-        iou = float(self.iouscale.get()) / 100      
-        
+        iou = float(self.iouscale.get()) / 100
+
         cap = cv2.VideoCapture(0)
         cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
         cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
@@ -276,7 +294,8 @@ class Predict(tk.Frame):
                 continue
 
             self.result_entry.delete(1.0, tk.END)
-            results = model.predict(frame, save=False, show=False, conf=confidence,imgsz=640)
+            results = model.predict(
+                frame, save=False, show=False, conf=confidence, imgsz=640)
             self.result_entry.insert(tk.END, results)
 
             cv2.imshow("frame", results[0].plot())
@@ -286,28 +305,42 @@ class Predict(tk.Frame):
         cap.release()
         cv2.destroyAllWindows()
 
- 
     def show_link_prediction_dialog(self):
         confidence = float(self.confscale.get()) / 100
         iou = float(self.iouscale.get()) / 100
         link = simpledialog.askstring("Link Prediction", "Enter the link for prediction:")
+        
         if self.model_file_path:
+            if not link:
+                messagebox.showwarning(title="Predict", message="No link provided.")
+                return
+            
             try:
-                if link:
-                    self.result_entry.delete(1.0, tk.END)
-                    results = YOLO(self.model_file_path).predict(source=link, show=True, save=True, conf=confidence,
-                                                                iou=iou,imgsz=640)
+                self.result_entry.delete(1.0, tk.END)
+                img = wget.download(link)
+                results = YOLO(self.model_file_path).predict(img, show=True, save=True, conf=confidence, iou=iou, imgsz=640)
+                
+                # Check if results contain any frames
+                if results:
                     results_text = f"File name {self.file_path}:\n\nSpeed {results[0].speed}:\n\nSaved Path {results[0].path}:\n"
                     self.result_entry.insert(tk.END, results_text)
                 else:
-                    messagebox.showwarning(title="Predict", message="Model file or input file not selected.")
+                    messagebox.showwarning(title="Predict", message="No frames received from the video stream.")
+            
             except Exception as er:
-                messagebox.showerror(title="Predict", message=str(er))
-    
+                # Handle specific exceptions related to video stream
+                if 'Video stream unresponsive' in str(er):
+                    messagebox.showerror(title="Predict", message="Video stream unresponsive, please check your IP camera connection.")
+                else:
+                    messagebox.showerror(title="Predict", message=str(er))
+        else:
+            messagebox.showwarning(title="Predict", message="Model file not selected.")
+
     def directoryprediction(self):
         confidence = float(self.confscale.get()) / 100
         iou = float(self.iouscale.get()) / 100
-        directory = filedialog.askdirectory(title="Select Directory for Prediction")
+        directory = filedialog.askdirectory(
+            title="Select Directory for Prediction")
 
         if self.model_file_path:
             try:
@@ -315,17 +348,21 @@ class Predict(tk.Frame):
                     self.result_entry.delete(1.0, tk.END)
                     model = YOLO(self.model_file_path)
                     results_text = ""
-                    
+
                     for filename in os.listdir(directory):
                         file_path = os.path.join(directory, filename)
                         if os.path.isfile(file_path):
-                            results = model.predict(source=file_path,  save=True, conf=confidence, iou=iou, imgsz=640)
-                            results_text += f"File name {filename}:\n\nSpeed {results[0].speed}:\n\nSaved Path {results[0].path}:\n"
+                            results = model.predict(
+                                source=file_path,  save=True, conf=confidence, iou=iou, imgsz=640)
+                            results_text += f"File name {filename}:\n\nSpeed {
+                                results[0].speed}:\n\nSaved Path {results[0].path}:\n"
 
                     self.result_entry.insert(tk.END, results_text)
                 else:
-                    messagebox.showwarning(title="Predict", message="No directory selected.")
+                    messagebox.showwarning(
+                        title="Predict", message="No directory selected.")
             except Exception as er:
                 messagebox.showerror(title="Predict", message=str(er))
         else:
-            messagebox.showwarning(title="Predict", message="Model file not selected.")
+            messagebox.showwarning(
+                title="Predict", message="Model file not selected.")
